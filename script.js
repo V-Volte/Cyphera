dd002a7abad46b543b7e13e0c49b88d4067f1340
Sekhara Pramod <32711469+V-Volte@users.noreply.github.com>
2022-11-20 01:04:11 +0530

    Generating plaintext and cyphertext divs

--- script.js
+++ script.js
@@ -16,6 +16,52 @@ let demos = [
 ]
 
 let encryptedDemos = [fibEncrypt(demos[0]), fibEncrypt(demos[1]), fibEncrypt(demos[2]), fibEncrypt(demos[3])];
+let msgcont = document.getElementById('msgcont');
+for (let i = 0; i < encryptedDemos.length; ++i) {
+
+    let row = document.createElement("div");
+    row.classList.add('row');
+    row.id = `row${i}`;
+
+    let pt = document.createElement("div");
+    pt.classList += "plain";
+    pt.id = `pt${i}`;
+    pt.innerText = demos[i];
+
+    let ct = document.createElement("div");
+    ct.classList += "cipher";
+    ct.id = `ct${i}`;
+    ct.innerText = encryptedDemos[i];
+
+    row.appendChild(ct);
+    row.appendChild(pt);
+
+    msgcont.appendChild(row);
+}
+
+let qcont = document.getElementById('qcont');
+for (let i = 0; i < sentences.length; ++i) {
+
+    let row = document.createElement("div");
+    row.classList.add('row');
+    row.id = `row${i}`;
+
+    let pt = document.createElement("input");
+    pt.type = "text"
+    pt.classList += "plaintext";
+    pt.id = `a${i}`;
+    pt.placeholder = 'Enter deciphered message';
+
+    let ct = document.createElement("div");
+    ct.classList += "cipher";
+    ct.id = `q${i}`;
+    ct.innerText = fibEncrypt(sentences[i]);
+
+    row.appendChild(ct);
+    row.appendChild(pt);
+
+    qcont.appendChild(row);
+}
 
 // for (let i = 0; i < 4; ++i) {
 //     encryptedDemos.push(fibEncrypt(demos[i]));
